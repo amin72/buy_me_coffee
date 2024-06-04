3a6166289726cc82f025118b52b2cfefb9b87294
Amin <asaa47721@gmail.com>
2024-06-04 21:28:16 +0330

    add media to project

--- buy_me_coffee/urls.py
+++ buy_me_coffee/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -13,4 +15,4 @@ urlpatterns = [
 
     # creator
     path("creator/", include('creator.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
